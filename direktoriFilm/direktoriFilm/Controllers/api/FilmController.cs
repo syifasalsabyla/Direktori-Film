7f8cb3fd403684690c55aac7cdf191eb63cc46a8
syifasalsabyla228@gmail.com <syifasalsabyla228@gmail.com>
2019-02-21 13:49:58 +0700

    Menambahkan folder dalam controller dengan nama 'API' dan isinya (FilmController.cs)

---
+++ direktoriFilm/direktoriFilm/Controllers/api/FilmController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using direktoriFilm.Data;
+using direktoriFilm.Models;
+
+namespace direktoriFilm.Controllers.api
+{
+    [Route("api/Film")]
+    [ApiController]
+    public class FilmController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FilmController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Film
+        [HttpGet]
+        public IEnumerable<Film> GetFilm()
+        {
+            return _context.Film;
+        }
+
+        // GET: api/Film/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetFilm([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var film = await _context.Film.FindAsync(id);
+
+            if (film == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(film);
+        }
+
+        // PUT: api/Film/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutFilm([FromRoute] int id, [FromBody] Film film)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != film.filmID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(film).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FilmExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Film
+        [HttpPost]
+        public async Task<IActionResult> PostFilm([FromBody] Film film)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Film.Add(film);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetFilm", new { id = film.filmID }, film);
+        }
+
+        // DELETE: api/Film/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteFilm([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var film = await _context.Film.FindAsync(id);
+            if (film == null)
+            {
+                return NotFound();
+            }
+
+            _context.Film.Remove(film);
+            await _context.SaveChangesAsync();
+
+            return Ok(film);
+        }
+
+        private bool FilmExists(int id)
+        {
+            return _context.Film.Any(e => e.filmID == id);
+        }
+    }
+}
