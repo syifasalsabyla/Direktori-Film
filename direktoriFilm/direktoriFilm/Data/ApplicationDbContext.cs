ad39736ba7c655b064372b5ee01ab9b4a8dd68fd
syifasalsabyla228@gmail.com <syifasalsabyla228@gmail.com>
2019-02-21 11:56:49 +0700

    Menambahkan Models dengan nama Film.cs

--- direktoriFilm/direktoriFilm/Data/ApplicationDbContext.cs
+++ direktoriFilm/direktoriFilm/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using direktoriFilm.Models;
 
 namespace direktoriFilm.Data
 {
@@ -12,5 +13,6 @@ namespace direktoriFilm.Data
             : base(options)
         {
         }
+        public DbSet<direktoriFilm.Models.Film> Film { get; set; }
     }
 }
---
+++ direktoriFilm/direktoriFilm/Models/Film.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace direktoriFilm.Models
+{
+    public class Film
+    {
+        public int filmID { get; set; }
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public int Year { get; set; }
+    }
+}
